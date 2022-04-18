4c43da45ad6366968c51762fb350e2920c8e590e
anhhatong <tonghaanh1999@gmail.com>
2022-04-17 23:54:01 -0400

    init :tada:

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // used in all the APIs
+  await app.listen(3000);
+}
+bootstrap();
