4afff03d97d1b972796586cb08e674327e6f2504
anhhatong <tonghaanh1999@gmail.com>
2022-04-18 16:37:32 -0400

    Integrated Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // used in all the APIs
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('NestJS Tutorial')
+    .setDescription('This is a blog series tutorial.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000); // localhost: 3000
 }
+
 bootstrap();
